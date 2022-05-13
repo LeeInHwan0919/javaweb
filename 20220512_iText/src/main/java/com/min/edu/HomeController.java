fb918aa6bfe6d8024db2bc7a275c8a4f85cfa82a
DESKTOP-OGEAKG6\GDJ44 <ldkkoj777@naver.com>
2022-05-13 17:21:54 +0900

    no message

--- 20220512_iText/src/main/java/com/min/edu/HomeController.java
+++ 20220512_iText/src/main/java/com/min/edu/HomeController.java
@@ -8,7 +8,7 @@ import java.util.Locale;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.ui.Model;import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -36,6 +36,12 @@ public class HomeController {
 		model.addAttribute("serverTime", formattedDate );
 		HTMLtoPDF.main(null);
 		return "home";
+	}
+	
+	@RequestMapping(value="/newFile.do", method=RequestMethod.GET)
+	public String newFile() {
+		
+	return "newFile";	
 	}
 	
 }
--- 20220512_iText/src/main/webapp/WEB-INF/views/home.jsp
+++ 20220512_iText/src/main/webapp/WEB-INF/views/home.jsp
@@ -12,5 +12,6 @@
 버튼을누르면 
 html을 pdf로 바꾸어지는 메소드를 동작
 <P>  The time on the server is ${serverTime}. </P>
+<a href="./newFile.do">포폴페이지이동</a>
 </body>
 </html>
